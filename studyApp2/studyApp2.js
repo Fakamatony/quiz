6126a820e250ec5c011b5e424da23a40cd5bcb88
Fakamatony <irn21bru.official@yandex.ru>
2022-05-11 12:09:39 +0300

    quiz displays the questions separately, after clicking the question changes P.S. The questions are very crooked

--- studyApp2/studyApp2.js
+++ studyApp2/studyApp2.js
@@ -1,31 +1,83 @@
-const name = document.getElementById('name');
-const surname = document.getElementById('surname');
-const age = document.getElementById('age');
-const color = document.getElementById('color');
-const description = document.getElementById('description');
+const questionsContainer = document.getElementById('questionsContainer')
+const btnStart = document.querySelector('.btnStart')
+let questions = [
+  {
+    question: "Какое пиво больше всего по душе?",
+    answers: [
+      {
+        answer: "Балтика"
+      },
+      {
+        answer: "Синий Майбах 3D"
+      },
+      {
+        answer: "Рижское"
+      },
+    ]
+  },
+  {
+    question: "Скотлько бутылок пива Синий Майбах 3D есть на земле?",
+    answers: [
+      {
+        answer: "4"
+      },
+      {
+        answer: "1000"
+      },
+      {
+        answer: "10000"
+      },
+    ]
+  },
+  {
+    question: "Вы сомневаетесь что выйграете приз?",
+    answers: [
+      {
+        answer: "Да"
+      },
+      {
+        answer: "Не"
+      },
+      {
+        answer: "Да мне есть 18"
+      },
+    ]
+  }
+];
 
 
-const sendBtn = document.getElementById('sendBtn');
 
-sendBtn.addEventListener('click', () => {
-    //Возвращает информацию массивом
-    const userInfo = {
-        name: name.value,
-        surname: surname.value,
-        age: age.value,
-        color: color.value,
-        description: description.value
+function createQuestionsAndAnswers(questionsList){
+
+
+  for (let i = 0; i < questionsList.length; i++){
+    const questionDiv = document.createElement('div');
+    questionDiv.classList.add('questionDiv')
+    const answersDiv = document.createElement('div');
+    const question = document.createElement('p');
+    question.innerHTML = questionsList[i].question;
+    for (let j = 0; j < questionsList[i].answers.length; j++){
+      const answer = document.createElement('button');
+      answer.classList.add('answer')
+      answer.innerHTML = questionsList[i].answers[j].answer;
+      answersDiv.append(answer)
+      answer.addEventListener('click', ()=>{
+        questionDiv.remove()
+      })
     }
-    console.log(userInfo)
-    //Возвращает тупо через консоль
-    console.log(name.value,surname.value,age.value,color.value,description.value)
 
-    name.value = ''
-    surname.value = ''
-    age.value = ''
-    color.value = "#rrggbb"
-    description.value = ''
+    questionDiv.append(question)
+    questionDiv.append(answersDiv)
+    questionsContainer.append(questionDiv)
+  }
+
+}
 
 
+
+btnStart.addEventListener('click', ()=> {
+  btnStart.remove();
+  createQuestionsAndAnswers(questions)
 })
+
 
